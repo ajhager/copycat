2c8d0c9df0efb1f22d9a4b9598fdc37658c8a2a2
Joseph Hager <ajhager@gmail.com>
2009-12-12 12:51:49 -0500

    Added opengl data and quick mockup of desired gui style.

--- clients/opengl/__init__.py
+++ clients/opengl/__init__.py
@@ -14,104 +14,79 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-from copycat.run import Run
-from copycat.coderack.codelets import *
-
-codelet_types = [AnswerBuilder, BondBottomUpScout, BondBuilder,
-                 BondStrengthTester, BondTopDownCategoryScout,
-                 BondTopDownDirectionScout, Breaker,
-                 CorrespondenceBottomUpScout, CorrespondenceBuilder,
-                 CorrespondenceImportantObjectScout,
-                 CorrespondenceStrengthTester, DescriptionBottomUpScout,
-                 DescriptionBuilder, DescriptionStrengthTester,
-                 DescriptionTopDownScout, GroupBuilder, GroupStrengthTester,
-                 GroupTopDownCategoryScout, GroupTopDownDirectionScout,
-                 GroupWholeStringScout, ReplacementFinder, RuleBuilder,
-                 RuleScout, RuleStrengthTester, RuleTranslator]
-
-class CoderackStatistics:
-    '''
-    How many of each codelet each step.
-    which bin they are in
-    how many of each codelet for the run
-    list of codelets that were run (by doing diffs)
-    '''
-    def __init__(self, coderack):
-        self.coderack = coderack
-        self.total_seen = self.empty_dict()
-        self.last_step = self.empty_dict()
-
-    def empty_dict(self):
-        d = {}
-        for codelet_type in codelet_types:
-            d[codelet_type] = 0
-        return d
-
-    def update(self):
-        this_step = self.empty_dict()
-        for codelet in self.coderack.codelets():
-            this_step[type(codelet)] += 1
-        for codelet_type in codelet_types:
-            change = this_step[codelet_type] - self.last_step[codelet_type]
-            if change > 0:
-                self.total_seen[codelet_type] += change
-        self.last_step = this_step
-
-
 import pyglet
+from pyglet.gl import *
+pyglet.resource.path.append("clients/opengl/data")
+pyglet.resource.reindex()
+pyglet.resource.add_font("erasdust.ttf")
+
+from copycat.run import Run
+
+class Letter(object):
+    def __init__(self, letter, x, y):
+        self.letter = letter
+        self.label = pyglet.text.Label(letter.name, "EraserDust", 36, x=x, y=y,
+                                       color=(255,255,255, 125))
+
+    def draw(self):
+        if self.letter.is_changed:
+            self.label.color = (255, 100, 100, 130)
+        else:
+            self.label.color = (255, 255, 255, 130)
+
+        self.label.draw()
 
 class Window(pyglet.window.Window):
-    def __init__(self, run, stats):
-        super(Window, self).__init__(800, 600)
-        self.run = run
-        self.coderack_stats = stats
-        pyglet.clock.schedule(self.update)
-        pyglet.gl.glClearColor(40/255.,44/255.,49/255.,1)
+    def __init__(self, run):
+        super(Window, self).__init__(1024, 600, caption="Copycat")
         self.clock = pyglet.clock.ClockDisplay()
+        self.done = False
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-        self.coderack = pyglet.text.document.FormattedDocument(" ")
-        self.coderack.set_style(0, 2, {'color':(92,141,152,255), 'font_name': "Monaco",
-                                       'font_size': 10, 'bold': True})
-        self.layout = pyglet.text.layout.TextLayout(self.coderack, 800, 600,multiline=True)
-        self.layout.x = 100
-        self.layout.y = -60
+        self.run = run
+        self.background = pyglet.resource.image("blackboard.png")
+
+        
+        self.letters = []
+        x, y = 80, 525
+        for letter in self.run.workspace.initial_string.get_letters():
+            self.letters.append(Letter(letter, x, y))
+            x += 50
+        x += 100
+        for letter in self.run.workspace.modified_string.get_letters():
+            self.letters.append(Letter(letter, x, y))
+            x += 50
+        x, y = 80, 400
+        for letter in self.run.workspace.target_string.get_letters():
+            self.letters.append(Letter(letter, x, y))
+            x += 50
+
+        pyglet.clock.schedule(self.update)
 
     def update(self, dt):
+        if self.done:
+            return
         if self.run.workspace.answer_string:
-            pyglet.app.exit()
+            self.done = True
+            x, y = 330, 400
+            for letter in self.run.workspace.answer_string.get_letters():
+                self.letters.append(Letter(letter, x, y))
+                x += 50
         self.run.step()
-        self.coderack_stats.update()
-        self.draw_coderack()
 
-    def draw_coderack(self):
-        text ='%s: %s' % ('Temperature', self.run.workspace.temperature)
-        self.set_caption(text)
-        text = ""
-        totals = self.coderack_stats.total_seen.items()
-        totals.sort(key=lambda x: x[1])
-        total = sum(self.coderack_stats.total_seen.values())
-        current = self.coderack_stats.last_step
-        total_current = len(self.run.coderack.codelets())
-        for (t, v) , y in zip(totals, range(len(totals))):
-            now = current[t]
-            text += "%40s: %5s %5s\n" % (t.__name__, v, now)
-        text += '\n%40s: %5s %5s' % ('Totals', total, total_current)
-
-        self.coderack.text = text
- 
     def on_draw(self):
         self.clear()
-        self.layout.draw()
+
+        self.background.blit(0, 0)
+        for letter in self.letters:
+            letter.draw()
+
         self.clock.draw()
 
 class OpenglClient(pyglet.window.Window):
     def __init__(self, initial, modified, target, seed):
         run = Run(initial, modified, target, seed)
-        coderack_stats = CoderackStatistics(run.coderack)
-        window = Window(run, coderack_stats)
+        window = Window(run)
         pyglet.app.run()
-        print "%5s %s %s %s" % (seed,
-                                str(run.workspace.answer_string.name),
-                                run.workspace.temperature,
-                                run.coderack.time)
 
